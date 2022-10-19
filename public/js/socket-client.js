0d6dd35b874dd3c6d21e6763140c17403f6e4eef
Herzfelder <dev.herzfelder>
2022-10-19 15:47:22 +0200

    socket controller

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,5 +1,7 @@
 const lblOnline = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
 
 
 
@@ -24,3 +26,29 @@ socket.on('disconnect', () => {
     
 
 });
+
+
+socket.on('enviar-mensaje', ( payload ) => {
+
+    console.log( payload )
+
+});
+
+
+
+
+btnEnviar.addEventListener( 'click', () => {
+
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+
+    socket.emit('enviar-mensaje', payload, ( id ) => {
+        console.log('Desde el servidor', id );
+    });
+
+
+}); 
